0d9cd430264ea0790a0ac9316a42df9cc551d160
Ibrahim Char <113177312+IbraChar03@users.noreply.github.com>
2023-04-07 16:36:05 +0200

    commit

--- csharp-oop-shop/Prodotto.cs
+++ csharp-oop-shop/Prodotto.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +10,51 @@ namespace csharp_oop_shop
 {
     internal class Prodotto
     {
+        private int codice;
+        //private string nome;
+        //private string descrizione;
+        //private int prezzo;
+        public string GetCodice()
+        {
+            Random rnd = new Random(); codice = rnd.Next(1, 99999999);
+            int left = (int) Math.Floor(Math.Log10(codice) + 1);
+            if (left < 8)
+            {
+               string codiceNuovo =  codice.ToString().PadLeft(5);
+                string parola = codice.ToString();
+                Console.WriteLine(parola.PadLeft(8,'0'));
+                Console.WriteLine(codice.ToString());
+                return codiceNuovo;
+              
+            }
+            return  codice.ToString();
+        }
+        public double prezzo { set; get; }
+        public double iva { set; get; }
+       
+        public string descrizione { set; get; }
+        public string nome { set; get; }
+        public double PrezzoBase()
+        {
+            return prezzo;
+        }
+        public double PrezzoIva()
+        {
+            return prezzo * iva;
+        }
+        public string NomeCompleto()
+        {
+            return nome + GetCodice();
+        }
+        public Prodotto(string nome, string descrizione, double prezzo, double iva)
+        {
+            this.nome = nome;
+            this.descrizione = descrizione;
+            this.prezzo = prezzo;
+            this.iva = iva;
+        }
+
+
+
     }
 }
--- csharp-oop-shop/Program.cs
+++ csharp-oop-shop/Program.cs
@@ -1,10 +1,18 @@
-﻿namespace csharp_oop_shop
+﻿using System;
+using System.Globalization;
+
+namespace csharp_oop_shop
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Prodotto prd1 = new Prodotto("Latte", "descrizione latte", 32, 1.22);
+            Console.WriteLine($"Il nome completo del prodotto è {prd1.NomeCompleto()}");
+            Console.WriteLine($"Il prezzo base del prodotto  è {prd1.PrezzoBase()}");
+            Console.WriteLine($"l'iva  è {prd1.iva}");
+            Console.WriteLine($"Il prezzo compreso di iva del prodotto  è {prd1.PrezzoIva()}");
+      
         }
     }
 }
